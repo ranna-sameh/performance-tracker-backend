f0ba6dafdd989ffbe7ff8cc962d54d4c5d5b39d9
rannasameh <rannasameh@hotmail.com>
2024-05-14 20:11:52 +0300

    Get ad by id

--- ad/urls.py
+++ ad/urls.py
@@ -1,8 +1,10 @@
 
-from .views import AdListView
+from .views import AdListView, AdDetailedView
 from django.urls import path
 
 urlpatterns = [
     path('', AdListView.as_view(), name='ads-list'),
+    path('<int:pk>', AdDetailedView.as_view(),
+         name='ad-details'),
 
 ]
